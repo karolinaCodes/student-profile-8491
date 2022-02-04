fa9474947e98095815ceb0936b6488a32ac20110
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-04 14:56:44 -0500

    implement expandable view of grades for students

--- src/components/StudentListItem.jsx
+++ src/components/StudentListItem.jsx
@@ -1,26 +1,48 @@
 import '../styles/StudentListItem.scss';
+import {useState} from 'react';
+
+// react-icons //
+import {AiOutlineMinusCircle, AiOutlinePlusCircle} from 'react-icons/ai';
 
 export default function StudentListItem(props) {
   const {email, company, skill, firstName, lastName, pic, grades} = props;
+  const [open, setOpen] = useState(false);
+  console.log(grades);
 
   const average = (
     grades.reduce((acc, curr) => +acc + +curr) / grades.length
   ).toFixed(2);
 
+  const formattedGrades = grades.map((grade, i) => (
+    <p key={i}>
+      Test {i + 1}: {grade}%
+    </p>
+  ));
+
   return (
     <li className="student-item">
-      <img src={pic} />
-      <div className="student-info">
-        <p className="student-name">
-          {firstName} {lastName}
-        </p>
-        <div className="student-details">
-          <p>Email: {email}</p>
-          <p>Company: {company}</p>
-          <p>Skill: {skill}</p>
-          <p>Average: {average}%</p>
+      <div className="student-data">
+        <img src={pic} />
+        <div className="student-info">
+          <p className="student-name">
+            {firstName} {lastName}
+          </p>
+          <div className="student-details">
+            <p>Email: {email}</p>
+            <p>Company: {company}</p>
+            <p>Skill: {skill}</p>
+            <p>Average: {average}%</p>
+            {open && <div className="grades">{formattedGrades}</div>}
+          </div>
         </div>
       </div>
+      <button className="expand-btn" onClick={() => setOpen(prev => !prev)}>
+        {open ? (
+          <AiOutlineMinusCircle size={50} />
+        ) : (
+          <AiOutlinePlusCircle size={50} />
+        )}
+      </button>
     </li>
   );
 }
